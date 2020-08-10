3e6b172b4bba6bbfc4fa526f02aff2155332466c
lien0103 <TP2003007@wistronits.com>
2020-08-10 15:34:00 +0800

    bluePrint settting

---
+++ .vscode/template/__pascalCase_name__/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './styles.module.scss';
+
+const {{pascalCase name}} = (props) =>{
+
+
+    return(
+        <>
+            
+        </>
+    )
+};
+
+export default {{pascalCase name}};
